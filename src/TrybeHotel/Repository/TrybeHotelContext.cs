bf67b57274ab8c07032cf2862f6df80e186c84ed
JuniorPastorini <juniorpastorini_@hotmail.com>
2024-02-27 02:03:15 -0300

    lint

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -7,16 +7,13 @@ public class TrybeHotelContext : DbContext, ITrybeHotelContext
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
     public TrybeHotelContext() { }
 
-    public DbSet<City> Cities { get; set; } = null!;
-    public DbSet<Hotel> Hotels { get; set; } = null!;
-    public DbSet<Room> Rooms { get; set; } = null!;
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Room> Rooms { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseSqlServer(connectionString);
-        }
+        optionsBuilder.UseSqlServer(connectionString);
     }
 }
